ed3f3fbad2bee3d74716c40c0ce9a8a5fbb06cf3
Saurabh Mishra <student001599@gmail.com>
2026-04-23 04:06:47 +0530

    cloudinary error

--- controllers/home.js
+++ controllers/home.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 const Servise = require("../models/services");
 const Course = require("../models/cources");
 const Blog = require("../models/blog");
-const cloudinary = require("../cloudConfig"); 
+const cloudinary = require("../Cloudconfig.js"); 
 const { all } = require("axios");
 
 module.exports.homePage = async (req, res, next) => {
